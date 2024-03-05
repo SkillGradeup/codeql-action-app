4b54ffb2acaa118072f318728c6d1477bf6e1c8f
sagarapidev <106273002+sagarapidev@users.noreply.github.com>
2024-03-05 07:53:23 +0530

    test

--- docs/modal/user/User.js
+++
@@ -1,8 +0,0 @@
-// UserModel.js
-const BaseModel = require("../BaseModel");
-
-class User extends BaseModel {
-  // Remove the constructor if it only calls the parent constructor
-}
-
-module.exports = User;
